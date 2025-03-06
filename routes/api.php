670d1f41160d0343ab8c6fd720ecd96ee6174d4e
Edgars G <Edgars@gmail>
2025-03-06 10:57:55 +0200

    Adding auth, vidPt2 12 min

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,23 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
-Route::resource('posts', PostController::class);
+// Route::resource('posts', PostController::class);
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 
-Route::get('/', function () {
-    return 'API';
-    });
+// Route::get('/', function () {
+//     return 'API';
+// });
+
+Route::apiResource('posts', PostController::class);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
